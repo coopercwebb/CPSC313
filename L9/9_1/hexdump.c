ab541634a6f89c8b415c63c042fe8cf0a177e56a
webb47 <webb47@students.cs.ubc.ca>
2024-11-23 16:27:59 -0800

    9_1 solution with printf to sout

--- L9/9_1/hexdump.c
+++ L9/9_1/hexdump.c
@@ -11,30 +11,42 @@
  * 2. outfile should not be created if infile does not exist
  */
 int hexdump(const char *infile, const char *outfile) {
-  // int fd_in;
-  // int fd_out;
-  // char buf[16];
 
-  // // opens file in read-only mode
-  // fd_in = open(infile, O_RDONLY);
-  // if (fd_in == -1) {
-  //     return 0;
-  // }
+    // opens file in read-only mode
+    int fd_in = open(infile, O_RDONLY);
+    if (fd_in == -1) {
+        return 0;
+    }
 
-  // fd_out = open(outfile, O_CREAT | O_WRONLY, 0644);
-  // if (fd_out == -1) {
-  //     close(fd_in);
-  //     return 0;
-  // }
+    int fd_out = open(outfile, O_WRONLY | O_CREAT | O_TRUNC);
+    if (fd_out == -1) {
+        close(fd_in);
+        return 0;
+    }
 
-  // int offset = 0;
-  // ssize_t bytesToRead;
+    int32_t offset = 0x0;
+    ssize_t bytes_read;
+    char buf[16];
 
-  // while ((bytesToRead = read(fd_in, buf, 16)) > 0) {
-  //     fprintf(fd_out, "%08x", offset);
-  //     }
+    while ((bytes_read = read(fd_in, buf, 16)) > 0) {
+        printf("%08x       ", offset);
+        printf("%x", buf[0] & 0xFF);
+        for (int i = 1; i < bytes_read; i++) {
+            if (i == 8) {
+                printf("    ");
+            } else {
+                printf(" ");
+            }
+            printf("%x", buf[i] & 0xFF);
+        }
+        printf("\n");
+        offset += bytes_read;
+    }
 
-  // }
+    printf("%08x\n", offset);
 
-  return 1;
+    close(fd_in);
+    close(fd_out);
+
+    return 1;
 }
