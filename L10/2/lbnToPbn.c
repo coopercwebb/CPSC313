c4cb15c65c1daf19e35bef100ed45acd67aea6ac
webb47 <webb47@students.cs.ubc.ca>
2024-11-26 18:51:01 -0800

    Nov 26 progress - failing large detection and hole detection

--- L10/2/lbnToPbn.c
+++ L10/2/lbnToPbn.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdint.h>
 #include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
 
 #include "lbnToPbn.h"
 
@@ -10,8 +10,7 @@
  * @arg index: the index to access within the block
  * @returns a block pointer located at the "index"th location inside "block"
  */
-static inline uint64_t get_pointer(block_t block, uint64_t index)
-{
+static inline uint64_t get_pointer(block_t block, uint64_t index) {
     return ((uint64_t *)&block.data)[index];
 }
 
@@ -24,7 +23,31 @@ static inline uint64_t get_pointer(block_t block, uint64_t index)
  *          BAD_PBN if lbn is a block beyond the end of a file
  *          HOLE_PBN if lbn is a "hole" in the file
  */
-uint64_t lbnToPbn(CS313inode_t *inode, uint64_t lbn, uint64_t block_size, block_t(*getBlock)(uint64_t block_no))
-{
-    return BAD_PBN;
+uint64_t lbnToPbn(CS313inode_t *inode, uint64_t lbn, uint64_t block_size, block_t (*getBlock)(uint64_t block_no)) {
+
+    uint64_t ptrsPerBlock = block_size / sizeof(uint64_t);
+
+    if (lbn < 8) {
+        // maps to direct block
+        return inode->i_direct[lbn];
+    }
+
+    uint64_t block_num = (lbn - 8) / ptrsPerBlock;
+    uint64_t block_index = (lbn - 8) % ptrsPerBlock;
+
+    if ((block_num + 8) * block_size >= inode->i_size) {
+        // too large of a lbn, error
+        return BAD_PBN;
+    }
+
+    block_t indirect_block = getBlock(inode->i_2indirect);
+    if (!indirect_block.is_valid) {
+        return HOLE_PBN;
+    }
+    uint64_t block_ptr = get_pointer(indirect_block, block_num);
+    block_t final_block = getBlock(block_ptr);
+    if (!final_block.is_valid) {
+        return HOLE_PBN;
+    }
+    return get_pointer(final_block, block_index);
 }
